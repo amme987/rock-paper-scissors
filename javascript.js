3436602e8720fe1b2da3d8e861db9dc59add0b70
amme987 <emmagination97@gmail.com>
2023-03-01 11:22:13 -0500

    Add score and results to UI

--- javascript.js
+++ javascript.js
@@ -25,42 +25,26 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === computerSelection) {
     tie++;
+    tieScore.textContent = "Tie: " + tie;
   } else if (
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     win++;
+    playerScore.textContent = "Player: " + win;
   } else {
     lose++;
+    computerScore.textContent = "Computer: " + lose;
   }
 
   // Announce winner of the game once one player reaches 5 points"
   if (win === 5) {
-    console.log("Congratulations! You won!");
+    results.textContent = "Congratulations! You won!";
   } else if (lose === 5) {
-    console.log("Sorry, you lost");
+    results.textContent = "Sorry, you lost";
   }
 }
-
-// function game() {
-//   let playerSelection;
-//   for (let i = 0; i < 5; i++) {
-//     playerSelection = prompt("Input rock, paper, or scissors");
-//     playerSelection = playerSelection.toLowerCase();
-//     playRound(playerSelection, getComputerChoice());
-//   }
-//   playerSelection = prompt("Input rock, paper, or scissors");
-//   playerSelection = playerSelection.toLowerCase();
-//   playRound(playerSelection, getComputerChoice());
-//   if ((tie > win && tie > lose) || (win == 2 && lose == 2)) {
-//     console.log("It's a tie!");
-//   } else if ((win > tie && win > lose) || (win == 2) & (tie == 2)) {
-//     console.log("Congratulations! You won!");
-//   } else {
-//     console.log("Sorry, you lost");
-//   }
-// }
 
 const buttons = Array.from(document.querySelectorAll("button"));
 buttons.forEach((button) =>
@@ -74,3 +58,8 @@ buttons.forEach((button) =>
     }
   })
 );
+
+const results = document.querySelector(".results");
+const playerScore = document.querySelector(".player");
+const computerScore = document.querySelector(".computer");
+const tieScore = document.querySelector(".tie");
